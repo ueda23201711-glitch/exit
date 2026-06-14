a86be2e4ac88f65b2af2a2d76579dbdb68ae2850
ueda23201711-glitch <ueda23201711@gmail.com>
2026-06-14 10:28:06 +0900

    PWA化、アイコン追加

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE = 'exit8-v1';
+const ASSETS = [
+  './exit8.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-180.png',
+  './icon-144.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
